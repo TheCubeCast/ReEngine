4be7a509e604a2354199d712e0d282a581f72eee
tecksup <tecksup@thecubecast.com>
2017-06-19 00:14:35 -0400

    Added fullscreen
    Matches your monitor resolution and sets the window decoration to nothing. No X button...

--- src/com/thecubecast/ReEngine/Main/game.java
+++ src/com/thecubecast/ReEngine/Main/game.java
@@ -9,16 +9,18 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
+import java.awt.Transparency;
 import javax.swing.JFrame;
 
 public class Game extends JFrame {
 
-	private BufferedImage Test;
+	private BufferedImage Player;
+	private BufferedImage Dirt;
 	
 	private int FPS = 120;
 	private String Title = "ReEngine";
-	private int Width = 600;
-	private int Height = 400;
+	private int Width = GetMonitorSizeW();
+	private int Height = GetMonitorSizeH();
 	
 	Insets insets;
 	InputHandler input;
@@ -39,6 +41,7 @@ public class Game extends JFrame {
 		
 		JukeBox.load("/Music/bgmusic.wav", "introsound");
 		JukeBox.loop("introsound");
+		//JukeBox.setVolume("introsound", -30.0f);
 		
 		while(isRunning) {
 			long time = System.currentTimeMillis();
@@ -65,7 +68,8 @@ public class Game extends JFrame {
 	void initialize() {
 		
 		try {
-			Test = ImageIO.read(getClass().getResourceAsStream("/Sprites/53.png"));
+			Player = ImageIO.read(getClass().getResourceAsStream("/Sprites/53.png"));
+			Dirt = ImageIO.read(getClass().getResourceAsStream("/Sprites/00.png"));
 		}
 		catch(Exception e) {
 			e.printStackTrace();
@@ -75,6 +79,8 @@ public class Game extends JFrame {
         setSize(Width, Height); 
         setResizable(false); 
         setDefaultCloseOperation(EXIT_ON_CLOSE); 
+        setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        setUndecorated(true);
         setVisible(true); 
         
         insets = getInsets(); 
@@ -106,18 +112,40 @@ public class Game extends JFrame {
 		{ 
 		        y += 5; 
 		} 
+		
+		//IF STATEMENT THAT WILL BE PUT HERE WHEN INPUT HANDLING IS FIXED
+		//IT WILL QUIT THE GAME
+		
 	}
 
 	
 	void draw() {
+		//This sets up the FPS and important stuff
 		Graphics g = getGraphics();
 		Graphics bbg = backBuffer.getGraphics();
-		
+		//Clears the screen each frame, Avoiding Artifacts
 		bbg.setColor(Color.WHITE);
 		bbg.fillRect(0, 0, Width, Height);
 		
-		bbg.drawImage(Test, x, y, 20, 20, null);
+		//The bottom layer
+		//Draw the background here
+		
+		//The Tiles are being drawn on this "Layer"
+		bbg.drawImage(Dirt, 20, 20, 40, 40, null);
+		
+		//The "Player" and other entities or overlays must be drawn last. Think top layer 
+		bbg.drawImage(Player, x, y, 20, 20, null);
 		
 		g.drawImage(backBuffer, insets.left, insets.top, this); 
+	}
+	
+	
+	int GetMonitorSizeW() {
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		return gd.getDisplayMode().getWidth();
+	}
+	int GetMonitorSizeH() {
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		return gd.getDisplayMode().getHeight();
 	}
 }
